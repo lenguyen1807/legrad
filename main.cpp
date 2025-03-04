3f7ab0f57707c041b72863b229df8e2819df6ad1
Le Nguyen <lenguyen18072003@gmail.com>
2025-03-04 15:35:37 +0700

    chore: testing on main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <stdexcept>
 
+#include "fmt/core.h"
+#include "fmt/format.h"
 #include "legrad/core/device.h"
+#include "legrad/internal/array_view.h"
 #include "legrad/internal/view_pack.h"
 #include "legrad/tensor/tensor_view.h"
 #include "legrad/util/helper_func.h"
+#include "macros/log.h"
 
 int main()
 {
-  legrad::ShapeAndStride view1(3);
-  legrad::ShapeAndStride view2(5);
-  legrad::ShapeAndStride view3(7);
-  view1.set_shape({1, 2, 3});
-  view2.set_shape({1, 2, 3, 4, 5});
-  std::cout << view1.numel() << "\n";
-  std::cout << view2.numel() << "\n";
+  legrad::ShapeAndStride vp(5);
+  std::cout << vp.shape_view() << "\n";
+  std::cout << vp.stride_view() << "\n";
+
+  legrad::internal::array_view<int> view = {1, 2, 3};
+  std::cout << view.at(0) << "\n";
+  std::cout << view.at(1) << "\n";
+  std::cout << view.at(2) << "\n";
+  std::cout << view << "\n";
+
   return 0;
 }
